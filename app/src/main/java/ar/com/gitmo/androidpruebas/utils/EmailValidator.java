6c12ae76dcd13c547545c2ced4f0e3b34a12a4f4
gcocce32 <gastoncocce@hotmail.com>
2016-09-13 10:49:27 -0300

    Agregamos un test para una clase EmailValidator

---
+++ app/src/main/java/ar/com/gitmo/androidpruebas/utils/EmailValidator.java
@@ -0,0 +1,17 @@
+package ar.com.gitmo.androidpruebas.utils;
+
+/**
+ * Created by gaston.cocce on 13/09/2016.
+ */
+public class EmailValidator {
+
+    public static boolean isValidEmail(String email){
+        if(email==null){
+            return false;
+        }
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+}
